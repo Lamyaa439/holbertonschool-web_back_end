11bfdaf0110cb041dd83f848aeaf3035f699dc6b
Lamyaa439 <lamyaaalghaihab@gmail.com>
2026-02-18 23:12:38 +0300

    Update

--- python_variable_annotations/9-element_length.py
+++ python_variable_annotations/9-element_length.py
@@ -8,6 +8,7 @@ from typing import List, Tuple
 
 def element_length(lst: List[str]) -> List[Tuple[str, int]]:
     """
-    
+    Annotate the below function's parameters and return values with
+    the appropriate types
     """
     return [(i, len(i)) for i in lst]
