1c475328d7c5b4974c4cebbe172a820b32eebceb
Lamyaa439 <lamyaaalghaihab@gmail.com>
2026-02-18 23:11:53 +0300

    Create 9-element_length.py

---
+++ python_variable_annotations/9-element_length.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+# 9-element_length.py
+"""
+This module have Annotate function's
+"""
+from typing import List, Tuple
+
+
+def element_length(lst: List[str]) -> List[Tuple[str, int]]:
+    """
+    
+    """
+    return [(i, len(i)) for i in lst]
