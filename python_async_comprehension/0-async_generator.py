ed40f72dce815b0340985d372afb4d92572ff7a2
Lamyaa439 <lamyaaalghaihab@gmail.com>
2026-02-22 00:02:23 +0300

    Update

--- python_async_comprehension/0-async_generator.py
+++ python_async_comprehension/0-async_generator.py
@@ -4,10 +4,10 @@ Module for asynchronous generator demonstration.
 """
 import asyncio
 import random
-from typing import AsyncGenerator
+from typing import Generator
 
 
-async def async_generator() -> AsyncGenerator[float, None, None]:
+async def async_generator() -> Generator[float, None, None]:
     """
     Asynchronous generator that yields 10 random numbers.
     """
